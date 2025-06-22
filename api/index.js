df1d11bf88f875aa65c1b8bdd1edd6985f09ac36
gopaybis <vlessgithub1@outlook.co.id>
2025-06-22 23:22:28 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,16 +1,15 @@
-export default async function handler(req, res) {
-  if (req.method === "POST") {
+module.exports = async (req, res) => {
+  if (req.method === 'POST') {
+    const { ip, port } = req.body;
     try {
-      const { ip, port } = req.body;
       const url = `https://apihealtcheck.vercel.app/api/v1?ip=${ip}&port=${port}`;
       const response = await fetch(url);
       const data = await response.json();
-      res.status(200).json(data);
-    } catch {
-      res.status(500).json({ error: true });
+      return res.status(200).json(data); // Mengembalikan hasil pengecekan
+    } catch (error) {
+      return res.status(500).json({ error: true, message: 'Failed to check proxy.' });
     }
   } else {
-    res.setHeader("Content-Type", "text/html");
-    res.sendFile("index.html", { root: process.cwd() + "/public" });
+    res.status(405).json({ error: 'Method Not Allowed' });
   }
-}
+};
