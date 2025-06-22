d0ec8e6526346d1cc55067e01a2d5324faa041a3
gopaybis <vlessgithub1@outlook.co.id>
2025-06-23 00:12:09 +0700

    Update check-proxy.js

--- api/check-proxy.js
+++ api/check-proxy.js
@@ -1,14 +1,20 @@
 // /api/check-proxy.js
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { ip, port } = req.body;
+const fetch = require('node-fetch');
+
+module.exports = async (req, res) => {
+  if (req.method === "POST") {
     try {
-      const response = await fetch(`https://apihealtcheck.vercel.app/api/v1?ip=${ip}&port=${port}`);
+      const { ip, port } = req.body;
+      const url = `https://apihealtcheck.vercel.app/api/v1?ip=${ip}&port=${port}`;
+      const response = await fetch(url);
       const data = await response.json();
-      return res.status(200).json(data);
+      
+      res.status(200).json(data);
     } catch (error) {
-      return res.status(500).json({ error: 'Internal server error' });
+      console.error(error);
+      res.status(500).json({ error: true });
     }
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
   }
-  return res.status(405).json({ error: 'Method Not Allowed' });
-}
+};
