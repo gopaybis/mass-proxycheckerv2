c693db7ab051afa3586ce1d2a4ceec3dff9bf0c3
gopaybis <vlessgithub1@outlook.co.id>
2025-06-22 23:27:16 +0700

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      const { ip, port } = req.body;
+      const url = `https://apihealtcheck.vercel.app/api/v1?ip=${ip}&port=${port}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      res.status(200).json(data);
+    } catch {
+      res.status(500).json({ error: true });
+    }
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.sendFile("index.html", { root: process.cwd() + "/public" });
+  }
+}
